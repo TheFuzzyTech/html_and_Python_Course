022b1b08df085b2798bffe1d688f655480c67a4e
Alex Snyder <asnyder@nfv.k12.ia.us>
2020-01-20 11:11:09 -0600

    Updated

---
+++ Python/04_Data_Types/test.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+# Can you guess these types:
+# 1
+x = "Hello World"
+print(type(x))
+# 2
+x = 5.1
+print(type(x))
+# 3
+x = 8675309
+print(type(x))
+# 4 
+x = ("Car", "Truck", "Helicopter")
+print(type(x))
+# 5
+x = {"name": "Alex", "Age": 24}
+print(type(x))
+# 6
+x = {"Car", "Truck", "Helicopter"}
+print(type(x))
+# 7
+x = [1,3,7,"8","Banana"]
+print(type(x))
+# 8
+x = True
+print(type(x))
